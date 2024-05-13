81b72a3856a94d840df94947d5718be6c86d15df
Hasan AlDoy <hd@doy.tech>
2024-05-13 14:08:15 +0000

    chore: update page title in index.php

--- index.php
+++ index.php
@@ -5,23 +5,25 @@
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
 
-function sayHello($name) {
+function sayHello($name)
+{
 	echo "Hello $name!";
 }
 
 ?>
 
 <html>
-	<head>
-		<title>Visual Studio Code Remote :: PHP</title>
-	</head>
-	<body>
-		<?php 
-		
-		sayHello('remote world');
-			
-		phpinfo(); 
-			
-		?>
-	</body>
+
+<head>
+	<title>هنا البحرين:: المطراش</title>
+</head>
+
+<body>
+	<?php
+
+
+
+	?>
+</body>
+
 </html>
